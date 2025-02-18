93ea6406b7fbe54c6be5fae2c9a6553bf42c456d
Max Champneys <max.champneys@sheffield.ac.uk>
2025-02-18 17:02:02 +0000

    -1, 1 feature range for MLP NARX models

--- trainers.py
+++ trainers.py
@@ -102,7 +102,7 @@ def GPNARX_trainer(data, key):
 
 
 def MLPNARX_trainer(data, key):
-    data, inv = scale_data(data, scaler=MM, scaler_params={"feature_range": (0, 1)})
+    data, inv = scale_data(data, scaler=MM, scaler_params={"feature_range": (-1, 1)})
     trains, vals, tests, opts = data
     n_batch = opts["n_batch"]
     lags = ARX_lag_scan(data, inv, n_batch, opts["max_lag"])
